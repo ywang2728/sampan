40ea0036ce3dcacb0ba135c37668c86c0f364ee0
ywang <yifan.wang@onespan.com>
2023-10-23 12:06:18 -0400

    Fixed putRec and tests.

--- ds/radix/radix_router_test.go
+++ ds/radix/radix_router_test.go
@@ -82,50 +82,137 @@ func TestStaticKeyMatchIterator(t *testing.T) {
 			assert.True(t, tp.HasNext())
 			assert.Equal(t, tc.tailPath.Next().(*staticKey).value, tp.Next().(*staticKey).value)
 		}
+	}
+}
+
+func TestStaticKeyMatch(t *testing.T) {
+	tcs := []struct {
+		value   string
+		path    string
+		tail    string
+		params  map[string]string
+		matched bool
+	}{
+		{value: "/", path: "/abc", tail: "abc", params: map[string]string{}, matched: true},
+		{value: "/abc", path: "/", tail: "/", params: map[string]string{}, matched: false},
+		{value: "/abc", path: "/ab", tail: "/ab", params: map[string]string{}, matched: false},
+		{value: "/123", path: "/123abc", tail: "abc", params: map[string]string{}, matched: true},
+		{value: "/123/", path: "/123/abc", tail: "abc", params: map[string]string{}, matched: true},
+		{value: "123/", path: "/123/abc", tail: "/123/abc", params: map[string]string{}, matched: false},
+		{value: "123", path: "/123/abc", tail: "/123/abc", params: map[string]string{}, matched: false},
+		{value: "123abc", path: "123/abc", tail: "123/abc", params: map[string]string{}, matched: false},
+		{value: "123/abc", path: "123/", tail: "123/", params: map[string]string{}, matched: false},
+	}
+	for _, tc := range tcs {
+		var sk Key[string] = &staticKey{value: tc.value}
+		tt, p, m := sk.Match(tc.path)
+		assert.Equal(t, tc.tail, tt)
+		assert.Equal(t, len(tc.params), len(p))
+		assert.Equal(t, tc.matched, m)
 
 	}
 }
 
-//func TestStaticKeyMatch(t *testing.T) {
-//	tcs := []struct {
-//		value   string
-//		path    string
-//		tail    string
-//		params  map[string]string
-//		matched bool
-//	}{
-//		{value: "/", path: "/abc", common: buildKeyIter("/"), tailKey: nil, tailPath: buildKeyIter("abc")},
-//		{value: "/abc", path: "/", common: buildKeyIter("/"), tailKey: buildKeyIter("abc"), tailPath: nil},
-//		{value: "/abc", path: "/ab", common: buildKeyIter("/ab"), tailKey: buildKeyIter("c"), tailPath: nil},
-//		{value: "/123", path: "/123abc", common: buildKeyIter("/123"), tailKey: nil, tailPath: buildKeyIter("abc")},
-//		{value: "/123/", path: "/123/abc", common: buildKeyIter("/123/"), tailKey: nil, tailPath: buildKeyIter("abc")},
-//		{value: "123/", path: "/123/abc", common: nil, tailKey: buildKeyIter("123/"), tailPath: buildKeyIter("/123/abc")},
-//		{value: "123", path: "/123/abc", common: nil, tailKey: buildKeyIter("123"), tailPath: buildKeyIter("/123/abc")},
-//		{value: "123abc", path: "123/abc", common: buildKeyIter("123"), tailKey: buildKeyIter("abc"), tailPath: buildKeyIter("/abc")},
-//		{value: "123/abc", path: "123/", common: buildKeyIter("123/"), tailKey: buildKeyIter("abc"), tailPath: nil},
-//	}
-//	for _, tc := range tcs {
-//		var sk Key[string] = &staticKey{value: tc.value}
-//		assert.Equal(t, tc.value, sk.Value())
-//		c, tk, tp, _ := sk.Match(&staticKey{value: tc.path})
-//		if tc.common == nil {
-//			assert.Nil(t, c)
-//		} else {
-//			assert.True(t, c.HasNext())
-//			assert.Equal(t, tc.common.Next().Value(), c.Next().Value())
-//		}
-//		if tc.tailKey == nil {
-//			assert.Nil(t, tk)
-//		} else {
-//			assert.True(t, tk.HasNext())
-//			assert.Equal(t, tc.tailKey.Next().Value(), tk.Next().Value())
-//		}
-//		if tc.tailPath == nil {
-//			assert.Nil(t, tp)
-//		} else {
-//			assert.True(t, tp.HasNext())
-//			assert.Equal(t, tc.tailPath.Next().Value(), tp.Next().Value())
-//		}
-//
-//	}
-//}
+// KeySeparator
+func TestNewKeySeparator(t *testing.T) {
+	tcs := []struct {
+		bgn string
+		end string
+	}{
+		{bgn: "(", end: ")"},
+		{bgn: ":", end: "/"},
+		{bgn: "{", end: "}"},
+	}
+	for _, tc := range tcs {
+		ks := newKeySeparator(tc.bgn, tc.end)
+		assert.NotNil(t, ks)
+		assert.Equal(t, ks.bs, tc.bgn)
+		assert.Equal(t, ks.es, tc.end)
+	}
+}
+
+func TestKeySeparatorOpenedAndClosed(t *testing.T) {
+	tcs := []struct {
+		bgn    string
+		end    string
+		char   string
+		opened bool
+		closed bool
+	}{
+		{bgn: "(", end: ")", char: "(", opened: true, closed: false},
+		{bgn: ":", end: "/", char: "(", opened: false, closed: true},
+		{bgn: "{", end: "}", char: "(", opened: false, closed: true},
+	}
+	for _, tc := range tcs {
+		ks := newKeySeparator(tc.bgn, tc.end)
+		assert.False(t, ks.opened())
+		assert.True(t, ks.closed())
+		ks.openWith(tc.char)
+		assert.Equal(t, tc.opened, ks.opened())
+		assert.Equal(t, tc.closed, ks.closed())
+	}
+}
+
+func TestKeySeparatorOpenWithAndCloseWith(t *testing.T) {
+	tcs := []struct {
+		bgn         string
+		end         string
+		bgnChars    []string
+		endChars    []string
+		closedTimes int
+		opened      bool
+		closed      bool
+	}{
+		{bgn: "(", end: ")", bgnChars: []string{"(", "("}, endChars: []string{")", ")"}, closedTimes: 0, opened: true, closed: true},
+		{bgn: ":", end: "/", bgnChars: []string{":"}, endChars: []string{"/"}, closedTimes: 0, opened: true, closed: true},
+		{bgn: "{", end: "}", bgnChars: []string{"{", "{"}, endChars: []string{"}", ")"}, closedTimes: 1, opened: true, closed: false},
+	}
+	for _, tc := range tcs {
+		ks := newKeySeparator(tc.bgn, tc.end)
+		var times int
+		var status bool
+		for _, c := range tc.bgnChars {
+			times, status = ks.openWith(c)
+		}
+		assert.Equal(t, len(tc.bgnChars), times)
+		assert.Equal(t, tc.opened, status)
+		for _, c := range tc.endChars {
+			times, status = ks.closeWith(c)
+		}
+		assert.Equal(t, tc.closedTimes, times)
+		assert.Equal(t, tc.closed, status)
+	}
+}
+
+func TestKeySeparatorOpenAndClose(t *testing.T) {
+	tcs := []struct {
+		bgn           string
+		end           string
+		bgnChars      []string
+		endChars      []string
+		closedTimes   int
+		opened        bool
+		closed        bool
+		isForceClosed bool
+	}{
+		{bgn: "{", end: "}", bgnChars: []string{"{", "{"}, endChars: []string{"}", ")"}, closedTimes: 0, opened: true, closed: false, isForceClosed: true},
+	}
+	for _, tc := range tcs {
+		ks := newKeySeparator(tc.bgn, tc.end)
+		var times int
+		var status bool
+		for range tc.bgnChars {
+			times, status = ks.open()
+		}
+		assert.Equal(t, len(tc.bgnChars), times)
+		assert.Equal(t, tc.opened, status)
+		for _, c := range tc.endChars {
+			times, status = ks.closeWith(c)
+		}
+		assert.Equal(t, tc.closed, status)
+		times, status = ks.close()
+		assert.Equal(t, tc.closedTimes, times)
+		assert.Equal(t, tc.isForceClosed, status)
+		assert.Equal(t, tc.isForceClosed, ks.closed())
+	}
+}
