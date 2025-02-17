1c8743371631ce32633cfffaea756a6c1aa7a09e
ywang <yifan.wang@onespan.com>
2025-02-17 01:11:33 -0500

    Update CasStack

--- ds/stack/stack.go
+++ ds/stack/stack.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strings"
 	"sync"
+	"sync/atomic"
 	"unsafe"
 )
 
@@ -14,55 +15,47 @@ type (
 	}
 
 	element[E any] struct {
-		prev  unsafe.Pointer
 		value E
 		next  unsafe.Pointer
 	}
 
 	MutexStack[E any] struct {
-		head *element[E]
-		tail *element[E]
-		mtx  sync.RWMutex
+		top unsafe.Pointer
+		len uint64
+		mtx sync.RWMutex
 	}
 
 	CasStack[E any] struct {
-		head *element[E]
+		top unsafe.Pointer
+		len uint64
 	}
 )
+
+//Mutex Stack
 
 func NewMutexStack[E any]() *MutexStack[E] {
 	return &MutexStack[E]{}
 }
 
-func NewCasStack[E any]() *CasStack[E] {
-	return &CasStack[E]{}
-}
-
 func (ms *MutexStack[E]) Push(e E) {
 	ms.mtx.Lock()
 	defer ms.mtx.Unlock()
-	elem := &element[E]{value: e}
-	if ms.tail == nil {
-		ms.head = elem
-	} else {
-		elem.prev = unsafe.Pointer(ms.tail)
-		ms.tail.next = unsafe.Pointer(elem)
+	elem := unsafe.Pointer(&element[E]{value: e})
+	if ms.top != nil {
+		(*element[E])(elem).next = ms.top
 	}
-	ms.tail = elem
+	ms.top = elem
+	ms.len++
 }
 
 func (ms *MutexStack[E]) Pop() (value E, ok bool) {
 	ms.mtx.Lock()
 	defer ms.mtx.Unlock()
-	if ms.tail != nil {
+	if ms.top != nil {
+		value = (*element[E])(ms.top).value
+		ms.top = (*element[E])(ms.top).next
+		ms.len--
 		ok = true
-		value = ms.tail.value
-		ms.tail = (*element[E])(ms.tail.prev)
-		if ms.tail == nil {
-			ms.head = nil
-		} else {
-			ms.tail.next = nil
-		}
 	}
 	return
 }
@@ -70,9 +63,9 @@ func (ms *MutexStack[E]) Pop() (value E, ok bool) {
 func (ms *MutexStack[E]) Peek() (value E, ok bool) {
 	ms.mtx.RLock()
 	defer ms.mtx.RUnlock()
-	if ms.tail != nil {
+	if ms.top != nil {
+		value = (*element[E])(ms.top).value
 		ok = true
-		value = ms.tail.value
 	}
 	return
 }
@@ -80,14 +73,76 @@ func (ms *MutexStack[E]) Peek() (value E, ok bool) {
 func (ms *MutexStack[E]) IsEmpty() bool {
 	ms.mtx.RLock()
 	defer ms.mtx.RUnlock()
-	return ms.head == nil
+	return ms.top == nil
 }
 
-func (ms *MutexStack[e]) String() string {
+func (ms *MutexStack[E]) String() string {
 	var sb strings.Builder
 	sb.WriteString("MutexStack[")
-	for curr := ms.head; curr != nil; curr = (*element[e])(curr.next) {
-		sb.WriteString(fmt.Sprintf("%v ", curr.value))
+	for curr := ms.top; curr != nil; curr = (*element[E])(curr).next {
+		sb.WriteString(fmt.Sprintf("%v ", (*element[E])(curr).value))
+	}
+	return strings.TrimRight(sb.String(), " ") + "]"
+}
+
+// CAS Stack
+
+func NewCasStack[E any]() *CasStack[E] {
+	return &CasStack[E]{}
+}
+
+func (cs *CasStack[E]) Push(e E) {
+	elem := &element[E]{value: e}
+	for {
+		if cs.top == nil {
+			atomic.StorePointer(&cs.top, unsafe.Pointer(elem))
+			return
+		}
+		old := atomic.LoadPointer(&cs.top)
+		elem.next = old
+		if atomic.CompareAndSwapPointer(&cs.top, old, unsafe.Pointer(elem)) {
+			cs.len++
+			return
+		}
+	}
+}
+
+func (cs *CasStack[E]) Pop() (value E, ok bool) {
+	for {
+		old := atomic.LoadPointer(&cs.top)
+		if old != nil {
+			oldElem := (*element[E])(old)
+			next := atomic.LoadPointer(&oldElem.next)
+			if atomic.CompareAndSwapPointer(&cs.top, old, next) {
+				cs.len--
+				return oldElem.value, true
+			}
+		}
+	}
+}
+
+func (cs *CasStack[E]) Peek() (value E, ok bool) {
+	for {
+		if cs.top == nil {
+			return value, false
+		} else {
+			old := atomic.LoadPointer(&cs.top)
+			return (*element[E])(old).value, true
+		}
+	}
+}
+
+func (cs *CasStack[E]) IsEmpty() bool {
+	for {
+		return atomic.LoadPointer(&cs.top) == nil
+	}
+}
+
+func (cs *CasStack[E]) String() string {
+	var sb strings.Builder
+	sb.WriteString("MutexStack[")
+	for curr := atomic.LoadPointer(&cs.top); curr != nil; curr = (*element[E])(curr).next {
+		sb.WriteString(fmt.Sprintf("%v ", (*element[E])(curr).value))
 	}
 	return strings.TrimRight(sb.String(), " ") + "]"
 }
--- ds/stack/stack_test.go
+++ ds/stack/stack_test.go
@@ -8,18 +8,30 @@ import (
 func TestMutexStackNew(t *testing.T) {
 	s := NewMutexStack[string]()
 	assert.NotNil(t, s)
-	assert.Nil(t, s.head)
-	assert.Nil(t, s.tail)
+	assert.Nil(t, s.top)
 }
 
-func TestStackIsEmpty(t *testing.T) {
+func TestCasStackNew(t *testing.T) {
+	s := NewCasStack[string]()
+	assert.NotNil(t, s)
+	assert.Nil(t, s.top)
+}
+
+func TestMutexStackIsEmpty(t *testing.T) {
 	s := NewMutexStack[string]()
 	assert.True(t, s.IsEmpty())
 	s.Push("haha")
 	assert.False(t, s.IsEmpty())
 }
 
-func TestStackPushPeekAndPop(t *testing.T) {
+func TestCasStackIsEmpty(t *testing.T) {
+	s := CasStack[string]{}
+	assert.True(t, s.IsEmpty())
+	s.Push("haha")
+	assert.False(t, s.IsEmpty())
+}
+
+func TestMutexStackPushPeekAndPop(t *testing.T) {
 	tcs := []struct {
 		pushList []string
 	}{
@@ -49,14 +61,62 @@ func TestStackPushPeekAndPop(t *testing.T) {
 	}
 }
 
-func TestStackString(t *testing.T) {
+func TestCasStackPushPeekAndPop(t *testing.T) {
+	tcs := []struct {
+		pushList []string
+	}{
+		{pushList: []string{}},
+		{pushList: []string{"a", "b", "c"}},
+		{pushList: []string{"abc", "bdd"}},
+	}
+	for _, tc := range tcs {
+		s := NewCasStack[string]()
+		for _, elem := range tc.pushList {
+			s.Push(elem)
+		}
+		reversed := make([]string, len(tc.pushList))
+		copy(reversed, tc.pushList[:])
+		for i, j := 0, len(reversed)-1; i < j; i, j = i+1, j-1 {
+			reversed[i], reversed[j] = reversed[j], reversed[i]
+		}
+		for _, elem := range reversed {
+			e, ok := s.Peek()
+			assert.True(t, ok)
+			assert.Equal(t, elem, e)
+			e, ok = s.Pop()
+			assert.True(t, ok)
+			assert.Equal(t, elem, e)
+		}
+		assert.True(t, s.IsEmpty())
+	}
+}
+
+func TestMutexStackString(t *testing.T) {
 	tcs := []struct {
 		pushList []string
 		output   string
 	}{
 		{pushList: []string{}, output: "MutexStack[]"},
-		{pushList: []string{"a", "b", "c"}, output: "MutexStack[a b c]"},
-		{pushList: []string{"abc", "bdd"}, output: "MutexStack[abc bdd]"},
+		{pushList: []string{"a", "b", "c"}, output: "MutexStack[c b a]"},
+		{pushList: []string{"abc", "bdd"}, output: "MutexStack[bdd abc]"},
+	}
+	for _, tc := range tcs {
+		s := NewMutexStack[string]()
+		for _, elem := range tc.pushList {
+			s.Push(elem)
+		}
+		assert.Equal(t, tc.output, s.String())
+	}
+}
+
+func TestCasStackString(t *testing.T) {
+	tcs := []struct {
+		pushList []string
+		output   string
+	}{
+		{pushList: []string{}, output: "MutexStack[]"},
+		{pushList: []string{"a", "b", "c"}, output: "MutexStack[c b a]"},
+		{pushList: []string{"abc", "bdd"}, output: "MutexStack[bdd abc]"},
 	}
 	for _, tc := range tcs {
 		s := NewMutexStack[string]()
